11b24ae8de8b176d0c19b2f354ea43f78d804574
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-01-27 19:56:25 -0500

    Start derivation

---
+++ shell.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/88a55dffa4d44d294c74c298daf75824dc0aafb5.tar.gz") {}
+}:
+let
+  stdenv = pkgs.gcc12Stdenv;
+in
+stdenv.mkDerivation rec {
+  pname = "srsran";
+  version = "24.10.1";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "srsran";
+    repo = "srsRAN_Project";
+    rev = "release_${builtins.replaceStrings [ "." ] [ "_" ] version}";
+    sha256 = "sha256-ufU6j/OuB+H0XlutIpPrgO9TCLzdDP5nDMAqQlDZ0Mk=";
+  };
+
+  nativeBuildInputs = with pkgs; [
+    cmake
+    pkg-config
+  ];
+
+  buildInputs = with pkgs; [
+    fftwFloat
+    mbedtls
+    boost
+    libconfig
+    lksctp-tools
+    pcsclite
+    uhd
+    soapysdr-with-plugins
+    libbladeRF
+    zeromq
+
+    fftw
+    yaml-cpp
+    mbedtls
+    gtest
+    lksctp-tools
+  ];
+
+  cmakeFlags = [ "-DENABLE_WERROR=OFF" ];
+}

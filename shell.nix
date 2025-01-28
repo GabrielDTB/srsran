8abf1b8395bb5a4ea6488644f9f985bceb67af33
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-01-27 21:49:33 -0500

    Turn into proper dev shell

--- shell.nix
+++ shell.nix
@@ -5,40 +5,44 @@
 let
   # Use Clang for faster compiles.
   stdenv = pkgs.llvmPackages.stdenv;
-in
-stdenv.mkDerivation rec {
-  pname = "srsran";
-  version = "24.10.1";
+  srsran = stdenv.mkDerivation rec {
+    pname = "srsran";
+    version = "24.10.1";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "srsran";
-    repo = "srsRAN_Project";
-    rev = "release_${builtins.replaceStrings [ "." ] [ "_" ] version}";
-    sha256 = "sha256-ufU6j/OuB+H0XlutIpPrgO9TCLzdDP5nDMAqQlDZ0Mk=";
-  };
+    src = pkgs.fetchFromGitHub {
+      owner = "srsran";
+      repo = "srsRAN_Project";
+      rev = "release_${builtins.replaceStrings [ "." ] [ "_" ] version}";
+      sha256 = "sha256-ufU6j/OuB+H0XlutIpPrgO9TCLzdDP5nDMAqQlDZ0Mk=";
+    };
 
-  nativeBuildInputs = with pkgs; [
-    cmake
-    pkg-config
+    nativeBuildInputs = with pkgs; [
+      cmake
+      pkg-config
+      # Use mold for faster linking.
+      mold
+    ];
+
+    buildInputs = with pkgs; [
+      fftwFloat
+      fftw
+      yaml-cpp
+      mbedtls
+      gtest
+      lksctp-tools
+    ];
+
     # Use mold for faster linking.
-    mold
-  ];
+    NIX_LDFLAGS = "-fuse-ld=mold";
+    CFLAGS = "-Wno-unused-command-line-argument";
+    CXXFLAGS = "-Wno-unused-command-line-argument";
 
-  buildInputs = with pkgs; [
-    fftwFloat
-    fftw
-    yaml-cpp
-    mbedtls
-    gtest
-    lksctp-tools
-  ];
-
-  # Use mold for faster linking.
-  NIX_LDFLAGS = "-fuse-ld=mold";
-  CFLAGS = "-Wno-unused-command-line-argument";
-  CXXFLAGS = "-Wno-unused-command-line-argument";
-
-  # Breaking purity is required because srsRAN compilation depends
-  # on the instruction set of the compiling computer.
-  NIX_ENFORCE_NO_NATIVE = "0";
+    # Breaking purity is required because srsRAN compilation depends
+    # on the instruction set of the compiling computer.
+    # This means that binaries *may not* be transferrable between computers.
+    NIX_ENFORCE_NO_NATIVE = "0";
+  };
+in
+pkgs.mkShell {
+  nativeBuildInputs = [ srsran ];
 }

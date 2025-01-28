8f07916fb39c129a100805c9ade7acbea464976a
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-01-27 20:59:51 -0500

    Snapshot first working build
    Fixed compilation issues by removing unnecessary dependencies and
    disabling NIX_ENFORCE_NO_NATIVE.

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/88a55dffa4d44d294c74c298daf75824dc0aafb5.tar.gz") {}
+{
+  # Pinned version of nixpkgs unstable on 2024/01/27.
+  pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/88a55dffa4d44d294c74c298daf75824dc0aafb5.tar.gz") {}
 }:
 let
+  # Use Clang for faster compiles.
   stdenv = pkgs.llvmPackages.stdenv;
 in
 stdenv.mkDerivation rec {
@@ -17,21 +20,12 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = with pkgs; [
     cmake
     pkg-config
+    # Use mold for faster linking.
     mold
   ];
 
   buildInputs = with pkgs; [
     fftwFloat
-    mbedtls
-    boost
-    libconfig
-    lksctp-tools
-    pcsclite
-    uhd
-    soapysdr-with-plugins
-    libbladeRF
-    zeromq
-
     fftw
     yaml-cpp
     mbedtls
@@ -39,9 +33,14 @@ stdenv.mkDerivation rec {
     lksctp-tools
   ];
 
+  # Use mold for faster linking.
   NIX_LDFLAGS = "-fuse-ld=mold";
   CFLAGS = "-Wno-unused-command-line-argument";
   CXXFLAGS = "-Wno-unused-command-line-argument";
+
+  # Breaking purity is required because srsRAN compilation depends
+  # on the instruction set of the compiling computer.
+  NIX_ENFORCE_NO_NATIVE = "0";
 
   cmakeFlags = [ "-DENABLE_WERROR=OFF" ];
 }
